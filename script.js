1621e5589e4cbefab2df939cbfe703bf1c6d6b2d
abed <abedhamami42@gmail.com>
2022-05-10 18:57:36 +0300

    added comments explaining the order gallery build

--- script.js
+++ script.js
@@ -92,19 +92,51 @@ let teslas = [
       }
   ];
 
+// example object of how a built in obj looks like
+// card = {
+// type : 'div',
+// classList : {classes:[], add : (className) => // class.push(className),}, 
+// innerHTML : '',
+// innerText: '',
+// style: {..., ..., backgroundImage: '', color: '', },
+// ...,
+// // +200
+}
 
 for (let i = 0; i < teslas.length; i++) {
 
-    let card = document.createElement('div');
+    let card = document.createElement('div'); // creating basic div === <div> </div>
 
-    card.classList.add('card');
+    card.classList.add('card'); // adding class to it ==== <div class="card" > </div>
+    //         our result so far
+    // card === <div class="card" > </div>
+     
+    card.innerHTML = `<h2> ${teslas[i].model} </h2>`;
 
-    // card.innerHTML = `<h2> ${teslas[i].model} </h2>`;
-    
+    // add an h2 tag in it === <div class="card"> <h2> model 3 </h2> </div>
+
     card.style.backgroundImage = `url(${teslas[i].img})`
+
+// change the style === 
+//  <div
+//  style="background-image: url('https://tesla-cdn.thron.com/delivery/public/image/tesla/32e5e0f3-5c04-42ef-8f8f-c6b1c26f8a9e/bvlatuR/std/2880x1800/ms-main-hero-desktop');" 
+//  class="card"> 
+// <h2> model 3 </h2> 
+// </div>
+
 
     document.querySelector('.gallery-grid').appendChild(card)
 
+// document.querySelector('.gallery-grid')  === targets the section in the html aka this one : <section class="gallery-grid"> </section>
+// after the append child method ==== below
 
+// <section class="gallery-grid"> 
 
+//  <div
+//  style="background-image: url('https://tesla-cdn.thron.com/delivery/public/image/tesla/32e5e0f3-5c04-42ef-8f8f-c6b1c26f8a9e/bvlatuR/std/2880x1800/ms-main-hero-desktop');" 
+//  class="card"> 
+// <h2> model 3 </h2> 
+// </div>
+
+// </section>
 }
